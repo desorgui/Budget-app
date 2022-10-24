43d9c48e49c0d29f92613990a2a1a49064011270
Desor Guishny <desorguishny@gmail.com>
2022-10-24 18:48:52 -0400

    Add ability cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,22 @@
 class Ability
   include CanCan::Ability
-  user ||= User.new # guest user (not logged in)
-  if user.admin?
+
+  def initialize(user)
+    return unless user.present?
+
+    can :manage, :all, user_id: user.id
+
+    return unless user.admin?
+      
     can :manage, :all
-  else
-    can :manage, Category, user_id: user.id
-    can :manage, Payment, user_id: user.id
-    can :read, :all
+
   end
+  # user ||= User.new # guest user (not logged in)
+  # if user.admin?
+  #   can :manage, :all
+  # else
+  #   can :manage, Category, user_id: user.id
+  #   can :manage, Payment, user_id: user.id
+  #   can :read, :all
+  # end
 end
