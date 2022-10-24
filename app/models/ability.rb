e1db097e942a59a117d4423ef14ec5b9a5d30fc8
Desor Guishny <desorguishny@gmail.com>
2022-10-24 18:52:47 -0400

    remove comment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,12 +11,4 @@ class Ability
     can :manage, :all
 
   end
-  # user ||= User.new # guest user (not logged in)
-  # if user.admin?
-  #   can :manage, :all
-  # else
-  #   can :manage, Category, user_id: user.id
-  #   can :manage, Payment, user_id: user.id
-  #   can :read, :all
-  # end
 end
